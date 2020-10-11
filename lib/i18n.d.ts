c4acf405efd9730bb2d8f054c6e037ac38d00fb3
Amin NAIRI <nairi.amin@gmail.com>
2020-10-11 12:01:21 +0200

    :tada: initial commit

--- lib/i18n.d.ts
+++ lib/i18n.d.ts
@@ -2,6 +2,6 @@ interface CreateI18nOptions {
     locale: Readonly<string>;
     translations: Readonly<Record<string, Record<string, string>>>;
 }
-export declare function createI18n({ locale, translations }: Readonly<CreateI18nOptions>): (strings: Readonly<string[]>, ...values: Readonly<string[]>) => string;
+export declare const createI18n: ({ locale, translations }: Readonly<CreateI18nOptions>) => (strings: Readonly<string[]>, ...values: Readonly<string[]>) => string;
 export {};
 //# sourceMappingURL=i18n.d.ts.map
--- lib/src/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const join: (first: Readonly<string[]>, second: Readonly<string[]>) => string;
-//# sourceMappingURL=utils.d.ts.map
