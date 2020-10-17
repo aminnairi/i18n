1b871e39fc141c3505ac1794a674e0c2e5dc5fe2
Amin NAIRI <nairi.amin@gmail.com>
2020-10-17 04:32:24 +0200

    :tada: initial commit

---
+++ examples/example.js
@@ -0,0 +1,20 @@
+const {createI18n} = require("@aminnairi/i18n");
+
+const translations = {
+    "Hello, world!": {
+        "es-ES": "¡Hola el mundo!",
+        "fr-FR": "Bonjour le monde!"
+    },
+    "Hi {}, You have {} unread messages.": {
+        "es-ES": "Buenos dias {}, tiene {} mensajes sin leer.",
+        "fr-FR": "Bonjour {}, vous avez {} messages non-lus."
+    }
+};
+
+const locale = "fr-FR";
+const i18n = createI18n({locale, translations});
+const name = "Amin NAIRI";
+const unreadMessages = 42;
+
+console.log(i18n`Hello, world!`);
+console.log(i18n`Hi ${name}, You have ${String(unreadMessages)} unread messages.`);
---
+++ examples/example.ts
@@ -0,0 +1,20 @@
+import {createI18n} from "@aminnairi/i18n";
+
+const translations: Record<string, Record<string, string>> = {
+    "Hello, world!": {
+        "es-ES": "¡Hola el mundo!",
+        "fr-FR": "Bonjour le monde!"
+    },
+    "Hi {}, You have {} unread messages.": {
+        "es-ES": "Buenos dias {}, tiene {} mensajes sin leer.",
+        "fr-FR": "Bonjour {}, vous avez {} messages non-lus."
+    }
+};
+
+const locale: string = "fr-FR";
+const i18n: (strings: Readonly<string[]>, ...values: Readonly<string[]>) => string = createI18n({locale, translations});
+const name: string = "Amin NAIRI";
+const unreadMessages: number = 42;
+
+console.log(i18n`Hello, world!`);
+console.log(i18n`Hi ${name}, You have ${String(unreadMessages)} unread messages.`);
