0f3083d89eb71d5f530f03aa5eae35912a587d6c
Amin NAIRI <nairi.amin@gmail.com>
2020-10-17 19:44:54 +0200

    :building_construction: added folder for each formats

---
+++ examples/browser/example.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const translations = {
+    "Hello, world!": {
+        "es-ES": "¡Hola el mundo!",
+        "fr-FR": "Bonjour le monde!"
+    },
+    "Hi {}, You have {} unread messages.": {
+        "es-ES": "Buenos dias {}, tiene {} mensajes sin leer.",
+        "fr-FR": "Bonjour {}, vous avez {} messages non-lus."
+    }
+};
+
+const locale = "fr-FR";
+const i18n = window.I18n.createI18n({locale, translations});
+const name = "Amin NAIRI";
+const unreadMessages = 42;
+
+console.log(i18n`Hello, world!`);
+console.log(i18n`Hi ${name}, You have ${String(unreadMessages)} unread messages.`);
+console.log(i18n`Good morning ${name}, what's on your mind today?`);
